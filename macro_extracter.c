2c5bc461d6beb0e542f393098652652d24b7e9f1
YitzchakKirschner <yitzchokkirshner@gmail.com>
2024-01-18 17:41:10 +0200

    add macro to macro list or lines to macros

--- macro_extracter.c
+++ macro_extracter.c
@@ -19,6 +19,35 @@ FILE* extractMacros(FILE* as_file_ptr, char* file_name){
             fclose(as_file_ptr);
             return NULL;
         }
-    
+
+    while (fgets(line, MAX_LINE_LENGTH, as_file_ptr)) {
+        if (in_macro_flag) {
+            if (isEndMacroDefinition(line)) {
+                in_macro_flag = 0;
+            } else {
+                addLineToMacro(currentMacro, line);
+                continue; /* No need to add to macro file */
+            }
+        }
+
+	/* Store the first word in the line into first_field*/
+        char firstField[MAX_MACRO_NAME_LENGTH];
+        sscanf(line, "%s", firstField);
+
+	/* If the first word is a name of a macro, replace it with the macro */
+        MacroNode *foundMacro = findMacro(macros, firstField);
+        if (foundMacro) {
+            // Replace macro with its lines
+            for (int i = 0; i < foundMacro->line_count; i++) {
+                fputs(foundMacro->lines[i], am_file_ptr);
+            }
+        } else if (isMacroDefinition(line)) {
+            in_macro_flag = 1;
+            currentMacro = addMacro(&macros, line + 4); // line + 4 skips "mcr "
+        } else {/* just a normal line */
+            fputs(line, am_file_ptr);
+        }
+    }
+
     return am_file_ptr;
 }
