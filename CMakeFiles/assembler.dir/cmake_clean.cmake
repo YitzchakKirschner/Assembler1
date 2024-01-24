1da6f6ecaf45788a52bbd2717ed158e75bc1a0e1
YitzchakKirschner <yitzchokkirshner@gmail.com>
2024-01-24 16:46:51 +0200

    seting up cmake

---
+++ CMakeFiles/assembler.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assembler.dir/errors.c.o"
+  "CMakeFiles/assembler.dir/errors.c.o.d"
+  "CMakeFiles/assembler.dir/macro_extracter.c.o"
+  "CMakeFiles/assembler.dir/macro_extracter.c.o.d"
+  "CMakeFiles/assembler.dir/main.c.o"
+  "CMakeFiles/assembler.dir/main.c.o.d"
+  "assembler"
+  "assembler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/assembler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
